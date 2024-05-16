827c993a3f5827de1dc14c2266e8595cc5656b74
Aditya Gupta <aditya27gupta@gmail.com>
2024-05-17 03:02:23 +0530

    Completed both parts of 1st Problem

--- 2015/1/main.py
+++ 2015/1/main.py
@@ -1,4 +1,4 @@
-def main(input_file: str) -> int:
+def find_last_floor(input_file: str) -> int:
     with open(input_file, mode='r') as file:
         string = file.read()
 
@@ -7,5 +7,27 @@ def main(input_file: str) -> int:
     return up_count - down_count
 
 
+def find_first_bottom_floor(input_file: str) -> int:
+    with open(input_file, mode='r') as file:
+        content = file.read()
+
+    total = 0
+    for idx, direction in enumerate(content):
+        if direction == '(':
+            total += 1
+        else:
+            total -= 1
+
+        if total < 0:
+            return idx + 1
+
+
+def main() -> None:
+    result = find_last_floor('input.txt')
+    print(result)
+    result = find_first_bottom_floor('input.txt')
+    print(result)
+
+
 if __name__ == "__main__":
-    print(main('input.txt'))
+    main()

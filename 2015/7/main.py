3c97f8c35ff5d011e14310cc6a2b1c33c930bd90
Aditya Gupta <aditya27gupta@gmail.com>
2024-05-18 17:41:49 +0530

    Started Day 7 Challenge

---
+++ 2015/7/main.py
@@ -0,0 +1,24 @@
+from typing import Dict, List
+
+
+def parse_instructions(instructions: List) -> Dict:
+    variables = {}
+    pass
+
+
+def main():
+    instruction: list = [
+        "123 -> x",
+        "456 -> y",
+        "x AND y -> d",
+        "x OR y -> e",
+        "x LSHIFT 2 -> f",
+        "y RSHIFT 2 -> g",
+        "NOT x -> h",
+        "NOT y -> i",
+    ]
+    pass
+
+
+if __name__ == "__main__":
+    main()

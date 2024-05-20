991f6118bd2a15cb41d03c03dbed955d4b59b01a
Aditya Gupta <aditya27gupta@gmail.com>
2024-05-20 15:25:52 +0530

    Completed Day 9 Challenge

---
+++ 2015/9/main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def find_shortest_path():
+    pass
+
+
+def main():
+    path: List[str] = [
+        "London to Dublin = 464",
+        "London to Belfast = 518",
+        "Dublin to Belfast = 141",
+    ]
+    pass
+
+
+if __name__ == "__main__":
+    main()
